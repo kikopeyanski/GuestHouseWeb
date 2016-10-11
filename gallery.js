d43f03abdfade0456c89194b985ed7a6e3e8311c
Xwz <kikopeyanski@gmail.com>
2016-10-11 19:28:53 +0300

    Added Map + other content
    -update

---
+++ gallery.js
@@ -0,0 +1,13 @@
+var cpo = [];
+cpo["_object"] = "cp_widget_6e48f5fd-5c20-479d-9af7-f064e30f0f80";
+cpo["_fid"] = "A4DA_udtJXvl";
+var _cpmp = _cpmp || [];
+_cpmp.push(cpo);
+(function () {
+    var cp = document.createElement("script");
+    cp.type = "text/javascript";
+    cp.async = true;
+    cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
+    var c = document.getElementsByTagName("script")[0];
+    c.parentNode.insertBefore(cp, c);
+})();
