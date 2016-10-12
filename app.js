83083d4368d08e00f105a7f815300a3cbe865699
Xwz <kikopeyanski@gmail.com>
2016-10-12 14:06:09 +0300

    Slight Navbar changes
    -fix

--- app.js
+++ app.js
@@ -5,12 +5,3 @@ $(window).scroll(function() {
         $('nav').removeClass('shrink');
     }
 });
-
-var htmlTemplate = "gallery.html";
-
-
-var btn= $("#gallery-load");
-btn.on("click",function () {
-    $("#gallery").load("Gallery-Slider/gallery.html");
-    console.log("succes")
-});
