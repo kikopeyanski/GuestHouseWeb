00f4940607d1b2046637bc61a15c461d12bbf893
Xwz <kikopeyanski@gmail.com>
2016-10-17 20:49:01 +0300

    Gallery Implemented
    -build

--- gallery.js
+++ gallery.js
@@ -1,13 +1,34 @@
-var cpo = [];
-cpo["_object"] = "cp_widget_6e48f5fd-5c20-479d-9af7-f064e30f0f80";
-cpo["_fid"] = "A4DA_udtJXvl";
-var _cpmp = _cpmp || [];
-_cpmp.push(cpo);
-(function () {
-    var cp = document.createElement("script");
-    cp.type = "text/javascript";
-    cp.async = true;
-    cp.src = "//www.cincopa.com/media-platform/runtime/libasync.js";
-    var c = document.getElementsByTagName("script")[0];
-    c.parentNode.insertBefore(cp, c);
-})();
+jQuery(document).ready(function ($) {
+
+    var jssor_1_options = {
+        $AutoPlay: true,
+        $SlideDuration: 800,
+        $SlideEasing: $Jease$.$OutQuint,
+        $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+        },
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+        }
+    };
+
+    var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+
+    /*responsive code begin*/
+    /*you can remove responsive code if you don't want the slider scales while window resizing*/
+    function ScaleSlider() {
+        var refSize = jssor_1_slider.$Elmt.parentNode.clientWidth;
+        if (refSize) {
+            refSize = Math.min(refSize, 1920);
+            jssor_1_slider.$ScaleWidth(refSize);
+        }
+        else {
+            window.setTimeout(ScaleSlider, 30);
+        }
+    }
+    ScaleSlider();
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    /*responsive code end*/
+});
