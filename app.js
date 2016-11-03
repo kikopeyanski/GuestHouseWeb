e728d006027faa888d3e3aa802ee8e681663e0c6
Xwz <kikopeyanski@gmail.com>
2016-11-04 00:19:52 +0200

    SecondGallery Fixed

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-$(".thumbnails1-img").on("click",function() {
-    var src = $(this).attr("src");
-    console.log(src);
-    $("#house1-image").attr("src", src);
-});
-$(".thumbnails2-img").on("click",function() {
-    var src = $(this).attr("src");
-    console.log(src);
-    $("#house2-image").attr("src", src);
-});
-
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('shrink');
