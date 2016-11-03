5d8b93d060d4cead61e509ae8ebc0cd0556d7b62
Xwz <kikopeyanski@gmail.com>
2016-11-03 17:39:20 +0200

    FIxed Houses

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+$(".thumbnails1-img").on("click",function() {
+    var src = $(this).attr("src");
+    console.log(src);
+    $("#house1-image").attr("src", src);
+});
+$(".thumbnails2-img").on("click",function() {
+    var src = $(this).attr("src");
+    console.log(src);
+    $("#house2-image").attr("src", src);
+});
+
 $(window).scroll(function() {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('shrink');
