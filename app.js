f5ec59841b136656bbb9bb0d0e9eeb10aa665536
Xwz <kikopeyanski@gmail.com>
2016-11-07 23:58:47 +0200

    Many Fixes
    -fix

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-$(window).scroll(function() {
+$(window).scroll(function () {
     if ($(document).scrollTop() > 50) {
         $('nav').addClass('shrink');
     } else {
         $('nav').removeClass('shrink');
     }
+});
+var pos = $("#jssor_2").offset();
+$("#static-image").offset({top: pos.top, left: 0});
+
+$('#map').on('click', function () {
+    $('#map iframe').css("pointer-events", "auto");
+    console.log("click")
+});
+
+$('#map').on('mouseleave', function () {
+    $('#map iframe').css("pointer-events", "none");
 });
