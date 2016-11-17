fc41e2ffd97fb0b9b45be71712ba100d05a46ae6
Xwz <kikopeyanski@gmail.com>
2016-11-17 16:30:39 +0200

    REM: Footer
    -beta

--- app.js
+++ app.js
@@ -25,9 +25,46 @@ $(document).click(function() {
     $('#general-terms').css('display','none');
 });
 $("#open-contact-form").click(function () {
-    $("#contact-form").css('display','inline');
+    $("#contact-form").css('display','inline-block');
 });
-$('.form-control').datepicker({
+$('.form-control-date').datepicker({
     format: "dd/mm/yyyy",
     language: "bg"
+});
+
+$("#home").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#jssor_1").offset().top
+    },1000);
+});
+$("#extras").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#info").offset().top -120
+    },1000);
+});
+$("#navigate-to-houses").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".houses-link-gallery").offset().top -120
+    },1000);
+});
+$("#navigate-to-gallery").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#jssor_2").offset().top -120
+    },1000);
+});
+$("#navigate-to-map").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#map").offset().top -120
+    },1000);
+});
+$("#navigate-to-price").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#price-and-contacts").offset().top -120
+    },1000);
+});
+$("#reserve").click(function() {
+    $("#contact-form").css('display','inline-block');
+    $('html, body').animate({
+        scrollTop: $("#price-and-contacts").offset().top -40
+    },1000);
 });
